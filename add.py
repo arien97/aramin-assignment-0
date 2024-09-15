ba1da3247a60a0e0b9f4a0a60010b7a0e6a43693
arien <aramin@bu.edu>
2024-09-15 17:43:22 -0400

    created python script to add two numbers

---
+++ add.py
@@ -0,0 +1,9 @@
+def add(num1, num2):
+    return num1 + num2
+
+number1 = float(input("First number: "))
+number2 = float(input("Second number: "))
+
+sum_result = add(number1, number2)
+
+print(sum_result)
